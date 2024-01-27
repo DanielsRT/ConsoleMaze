d7f29216a9fd56ef0e672da79820275948ed2a12
Robert Daniels <robdaniels2001@gmail.com>
2024-01-27 08:36:19 -0500

    Create screen buffer

--- source.cpp
+++ source.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 using namespace std;
 
+#include <Windows.h>
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
 int main()
 {
+    // Create Screen buffer
+    wchar_t *screen = new wchar_t[nScreenHeight * nScreenHeight];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
 
-    cout << "Hello World!\n";
+    screen[nScreenHeight * nScreenHeight - 1] = '\0';
+    WriteConsoleOutputCharacter(hConsole, screen, nScreenHeight * nScreenHeight, { 0,0 }, &dwBytesWritten);
 
     return 0;
 }
